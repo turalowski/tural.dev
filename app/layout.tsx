90c10afcdf446bb55e4d6a927a89d7fbdaadbd26
Tural Hajiyev <thajiyev9@outlook.com>
2023-12-04 22:27:26 +0100

    Initial version

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import { Source_Code_Pro } from 'next/font/google';
+import { ThemeProvider } from '@/app/components/theme-provider';
+import './globals.css';
+
+const source_code_pro = Source_Code_Pro({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Tural Hajiyev',
+  description:
+    'Personal website of Tural Hajiyev. Software deveveloper specialized on frontend development.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={source_code_pro.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Navbar from '@/app/components/navbar';
+import ToggleAppearance from '@/app/components/toggle-appearance';
+import Education from '@/app/components/education';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col  justify-start  p-6">
+      <nav className="flex items-center justify-between ">
+        <Navbar />
+        <ToggleAppearance />
+      </nav>
+      <div className="flex flex-col my-3 gap-3">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          Tural Hajiyev
+        </h2>
+        <Education />
+      </div>
+    </main>
+  );
+}
