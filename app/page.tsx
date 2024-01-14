8f7e4034ece9fcd4677ed4dff99ac9d2cb3f6e46
Tural Hajiyev <thajiyev9@outlook.com>
2024-01-14 23:43:51 +0100

    minimized

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,61 @@
+import { TwitterLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons';
+import { IoDocumentText } from 'react-icons/io5';
+import { Button } from './components/ui/button';
+import Link from 'next/link';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './components/ui/tooltip';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  justify-center items-center  p-6">
-      Hello
+    <main className="flex min-h-screen flex-col  justify-center items-center p-6">
+      <div className="flex flex-col items-center gap-3">
+        <div>Hello, I am Tural 👋</div>
+        <div className="flex items-center gap-3">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link target="_blank" href="https://github.com/turalowski">
+                  <Button variant="outline" size="icon">
+                    <GitHubLogoIcon />
+                    <span className="sr-only">Github</span>
+                  </Button>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>GitHub</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          {/* <TooltipProvider> */}
+          {/*   <Tooltip> */}
+          {/*     <TooltipTrigger asChild> */}
+          {/*       <Link target="_blank" href="https://github.com/turalowski"> */}
+          {/*         <Button variant="outline" size="icon"> */}
+          {/*           <IoDocumentText /> */}
+          {/*           <span className="sr-only">Github</span> */}
+          {/*         </Button> */}
+          {/*       </Link> */}
+          {/*     </TooltipTrigger> */}
+          {/*     <TooltipContent>CV</TooltipContent> */}
+          {/*   </Tooltip> */}
+          {/* </TooltipProvider> */}
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link target="_blank" href="https://twitter.com/turalowski">
+                  <Button variant="outline" size="icon">
+                    <TwitterLogoIcon />
+                    <span className="sr-only">Twitter</span>
+                  </Button>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>Twitter</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </div>
     </main>
   );
 }
