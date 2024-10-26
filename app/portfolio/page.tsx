39ed364a11a4db04b9f113834306aa8cae6b4d6d
Tural Hajiyev <thajiyev9@outlook.com>
2024-10-27 01:03:09 +0200

    Third project

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -7,50 +7,69 @@ import { ArrowLeftIcon, GitHubLogoIcon } from '@radix-ui/react-icons';
 
 export default function PortfolioPage() {
   const [fullscreenImage, setFullscreenImage] = useState<string | null>(null);
-  const portfolioItems = [
+  const portfolioProjects = [
     {
       id: 1,
-      title: 'Technote Media',
-      image: '/images/technote/main-dark.png',
-      description: 'Landing Page | Dark Theme',
+      title: 'Technote Media (Frontend)',
+      images: [
+        {
+          src: '/images/technote/main-dark.png',
+          description: 'Landing Page | Dark Theme',
+        },
+        {
+          src: '/images/technote/post-dark.png',
+          description: 'Post Page | Dark Theme',
+        },
+        {
+          src: '/images/technote/contact-dark.png',
+          description: 'Contact Page | Dark Theme',
+        },
+      ],
     },
     {
       id: 2,
-      title: 'Technote Media',
-      image: '/images/technote/post-dark.png',
-      description: 'Post Page | Dark Theme',
+      title: 'Prospect SMB (Frontend)',
+      images: [
+        {
+          src: '/images/prospect/order/orders.jpg',
+          description: 'Orders Module | List All Orders',
+        },
+        {
+          src: '/images/prospect/order/detail.png',
+          description: 'Orders Module | Order Details',
+        },
+        {
+          src: '/images/prospect/sales/sales.jpg',
+          description: 'Sales Module | List All Sales Operations',
+        },
+        {
+          src: '/images/prospect/sales/new-operation.png',
+          description: 'Sales Module | New Sales Operation',
+        },
+      ],
     },
     {
-      id: 3,
-      title: 'Technote Media',
-      image: '/images/technote/contact-dark.png',
-      description: 'Contact Page | Dark Theme',
-    },
-    {
-      id: 4,
-      title: 'Prospect SMB',
-      image: '/images/prospect/order/orders.jpg',
-      description: 'Orders Module | List All Orders',
-    },
-    {
-      id: 4,
-      title: 'Prospect SMB',
-      image: '/images/prospect/order/detail.png',
-      description: 'Orders Module | Order Details',
-    },
-    {
-      id: 5,
-      title: 'Prospect SMB',
-      image: '/images/prospect/sales/sales.jpg',
-      description: 'Sales Module | List All Sales Operations',
-    },
-    {
-      id: 6,
-      title: 'Prospect SMB',
-      image: '/images/prospect/sales/new-operation.png',
-      description: 'Sales Module | New Sales Operation',
-    },
-
+        id: 3,
+        title: 'TOLQ - Email Archive (Design)',
+        images: [
+            {
+                src: '/images/tolq/main.png',
+                description: 'Show All Emails'
+            },
+            {
+                src: '/images/tolq/archive.png',
+                description: 'Show All Archives'
+            },
+            {
+                src: '/images/tolq/content.png',
+                description: 'Show Content',
+            },
+            {
+                src: '/images/tolq/forward-email.png',
+                description: 'Forward Email',
+            }
+        ]
+    }
     // Add more projects as needed
   ];
 
@@ -73,27 +92,32 @@ export default function PortfolioPage() {
           <GitHubLogoIcon className="h-4 w-4" />
         </Button>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 mt-8">
-        {portfolioItems.map(item => (
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mt-8">
+        {portfolioProjects.map(project => (
           <div
-            key={item.id}
+            key={project.id}
             className="bg-white rounded-lg shadow-md overflow-hidden"
           >
-            <div
-              onClick={() => openFullscreen(item.image)}
-              className="cursor-pointer"
-            >
-              <Image
-                src={item.image}
-                alt={item.title}
-                width={150}
-                height={200}
-                className="w-full h-40 object-contain object-center transition-transform duration-300 hover:scale-105"
-              />
-            </div>
-            <div className="p-2">
-              <h2 className="text-sm font-semibold mb-1">{item.title}</h2>
-              <p className="text-xs text-gray-600">{item.description}</p>
+            <div className="p-4">
+              <h2 className="text-lg font-semibold mb-4">{project.title}</h2>
+              <div className="grid grid-cols-2 gap-4">
+                {project.images.map((image, index) => (
+                  <div
+                    key={index}
+                    onClick={() => openFullscreen(image.src)}
+                    className="cursor-pointer"
+                  >
+                    <Image
+                      src={image.src}
+                      alt={`${project.title} - ${image.description}`}
+                      width={100}
+                      height={75}
+                      className="w-full h-12 object-cover object-center transition-transform duration-300 hover:scale-105"
+                    />
+                    <p className="text-xs text-gray-600 mt-2">{image.description}</p>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         ))}
