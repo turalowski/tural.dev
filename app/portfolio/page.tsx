7d09f6164019ad6a9685f78300844cf095a23ad2
Tural Hajiyev <thajiyev9@outlook.com>
2024-10-27 18:16:56 +0100

    Final version

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -14,62 +14,172 @@ export default function PortfolioPage() {
       images: [
         {
           src: '/images/technote/main-dark.png',
-          description: 'Landing Page | Dark Theme',
+          description: 'Home Page (Dark Theme)',
+        },
+        {
+          src: '/images/technote/main-light.png',
+          description: 'Home Page (Light Theme)', 
         },
         {
           src: '/images/technote/post-dark.png',
-          description: 'Post Page | Dark Theme',
+          description: 'Blog Post (Dark Theme)',
         },
         {
-          src: '/images/technote/contact-dark.png',
-          description: 'Contact Page | Dark Theme',
+          src: '/images/technote/post-light.png',
+          description: 'Blog Post (Light Theme)',
         },
       ],
     },
     {
       id: 2,
-      title: 'Prospect SMB (Frontend)',
+      title: 'Prospect SMB - Contacts Module',
       images: [
         {
-          src: '/images/prospect/order/orders.jpg',
-          description: 'Orders Module | List All Orders',
+          src: '/images/prospect/contacts/contacts.png',
+          description: 'Contacts Dashboard',
         },
         {
-          src: '/images/prospect/order/detail.png',
-          description: 'Orders Module | Order Details',
+          src: '/images/prospect/contacts/new-contact.png',
+          description: 'Create Contact Form',
         },
         {
-          src: '/images/prospect/sales/sales.jpg',
-          description: 'Sales Module | List All Sales Operations',
+          src: '/images/prospect/contacts/partners.png',
+          description: 'Partners List',
         },
         {
-          src: '/images/prospect/sales/new-operation.png',
-          description: 'Sales Module | New Sales Operation',
+          src: '/images/prospect/contacts/new-partner.png',
+          description: 'Create Partner Form',
         },
       ],
     },
     {
-        id: 3,
-        title: 'TOLQ - Email Archive (Design)',
-        images: [
-            {
-                src: '/images/tolq/main.png',
-                description: 'Show All Emails'
-            },
-            {
-                src: '/images/tolq/archive.png',
-                description: 'Show All Archives'
-            },
-            {
-                src: '/images/tolq/content.png',
-                description: 'Show Content',
-            },
-            {
-                src: '/images/tolq/forward-email.png',
-                description: 'Forward Email',
-            }
-        ]
-    }
+      id: 3,
+      title: 'Prospect SMB - Orders Module',
+      images: [
+        {
+          src: '/images/prospect/orders-module/list-of-goods.png',
+          description: 'Products Catalog',
+        },
+        {
+          src: '/images/prospect/orders-module/orders.jpg',
+          description: 'Orders Dashboard',
+        },
+        {
+          src: '/images/prospect/orders-module/detail.png',
+          description: 'Order Information',
+        },
+        {
+          src: '/images/prospect/orders-module/timeline.png',
+          description: 'Order Status Timeline',
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: 'Prospect SMB - Sales Module',
+      images: [
+        {
+          src: '/images/prospect/sales/sales.jpg',
+          description: 'Sales Dashboard',
+        },
+        {
+          src: '/images/prospect/sales/new-operation.png',
+          description: 'Create Sale',
+        },
+        {
+          src: '/images/prospect/sales/detail.png',
+          description: 'Sale Details',
+        },
+        {
+          src: '/images/prospect/sales/invoice-content.png',
+          description: 'Invoice Preview',
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: 'Prospect SMB - Reports Module',
+      images: [
+        {
+          src: '/images/prospect/reports/sales-reports.png',
+          description: 'Sales Analytics',
+        },
+        {
+          src: '/images/prospect/reports/turnover.png',
+          description: 'Revenue Overview',
+        },
+        {
+          src: '/images/prospect/reports/balance-sheet.png',
+          description: 'Balance Sheet',
+        },
+        {
+          src: '/images/prospect/reports/balance-sheet-range.png',
+          description: 'Custom Date Range',
+        },
+      ],
+    },
+    {
+      id: 6,
+      title: '10BE5 - Landing Page',
+      images: [
+        {
+          src: '/images/10be5/home.png',
+          description: 'Welcome Page',
+        },
+        {
+          src: '/images/10be5/product.png',
+          description: 'Product Details',
+        },
+        {
+          src: '/images/10be5/contact.png',
+          description: 'Get in Touch',
+        },
+      ],
+    },
+    {
+      id: 7,
+      title: 'Tender',
+      images: [
+        {
+          src: '/images/tender/home.png',
+          description: 'Landing Page',
+        },
+        {
+          src: '/images/tender/categories.png',
+          description: 'Browse Categories',
+        },
+        {
+          src: '/images/tender/about-tender.png',
+          description: 'About Us',
+        },
+        {
+          src: '/images/tender/create-tender.png',
+          description: 'New Tender Form',
+        },
+      ],
+    },
+    {
+      id: 8,
+      title: 'Email Archive',
+      images: [
+        {
+          src: '/images/tolq/main.png',
+          description: 'Email Inbox',
+        },
+        {
+          src: '/images/tolq/archive.png',
+          description: 'Archived Emails',
+        },
+        {
+          src: '/images/tolq/content.png',
+          description: 'Email Content',
+        },
+        {
+          src: '/images/tolq/forward-email.png',
+          description: 'Forward Message',
+        },
+      ],
+    },
     // Add more projects as needed
   ];
 
@@ -84,11 +194,11 @@ export default function PortfolioPage() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex items-center gap-3">
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" onClick={() => window.location.href = '/'}>
           <ArrowLeftIcon className="h-4 w-4" />
         </Button>
         <h1 className="text-2xl font-bold">My Portfolio</h1>
-        <Button variant="outline" size="icon" className="ml-auto">
+        <Button variant="outline" size="icon" className="ml-auto" onClick={() => window.location.href = 'https://github.com/turalowski'}>
           <GitHubLogoIcon className="h-4 w-4" />
         </Button>
       </div>
@@ -99,7 +209,7 @@ export default function PortfolioPage() {
             className="bg-white rounded-lg shadow-md overflow-hidden"
           >
             <div className="p-4">
-              <h2 className="text-lg font-semibold mb-4">{project.title}</h2>
+              <h2 className="text-md font-semibold mb-4">{project.title}</h2>
               <div className="grid grid-cols-2 gap-4">
                 {project.images.map((image, index) => (
                   <div
@@ -114,7 +224,9 @@ export default function PortfolioPage() {
                       height={75}
                       className="w-full h-12 object-cover object-center transition-transform duration-300 hover:scale-105"
                     />
-                    <p className="text-xs text-gray-600 mt-2">{image.description}</p>
+                    <p className="text-xs text-gray-600 mt-2">
+                      {image.description}
+                    </p>
                   </div>
                 ))}
               </div>
